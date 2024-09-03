1f3d54df29f858ba2230c02984821de1ea8d6fa9
Evgenij Spitsyn <spev@gurtam.com>
2024-09-03 10:08:05 +0300

    manual date input

--- src/components/QTimestampPicker.vue
+++ src/components/QTimestampPicker.vue
@@ -4,18 +4,22 @@
       v-if="withInput"
       outlined
       dense
-      v-model="datetime"
-      @focus="focusemit"
-      @click="show"
+      :value="datetime"
+      @change="datetime = $event.target.value"
+      @input="applyInput"
       :disable="disable"
-      readonly
       :label="label"
       :suffix="suffix"
       :color="color"
       :error="error"
-    >
+      @focus="focusemit"
+      @blur="(ev) => { parseDate(datetime) }"
+      :readonly="!editableInput"
+      @click="!editableInput ? show : () => {}"
+      :class="!editableInput ? 'cursor-pointer' : ''"
+      >
       <template v-slot:append>
-        <q-icon name="mdi-calendar" class="cursor-pointer" @click="show" :color="color" />
+        <q-btn flat round dense icon="mdi-calendar" class="cursor-pointer" @click="show" :color="color" />
       </template>
     </q-input>
     <q-btn
@@ -39,15 +43,46 @@
       transition-hide="scale">
       <q-card class="q-pa-none">
         <q-card-section class="q-pa-none">
-          <div :class="`bg-${color} absolute-top-left absolute-top-right`" style="min-height:86px" />
+          <div :class="`bg-${color} absolute-top-left`" style="min-height:86px" />
           <div class="row items-start">
+            <div :class="`fit row ${splitView?'gt-xs' : ''}`" v-if="splitView">
+              <q-date
+                class="col-6"
+                style="width:270px"
+                flat
+                square
+                v-model="proxydatetime"
+                :mask="format"
+                @input="slide='time'"
+                :color="color"
+                :options="checkDay"
+                no-unset
+              />
+              <q-time
+                class="col-6"
+                style="width:270px"
+                flat
+                square
+                v-model="proxydatetime"
+                :mask="format"
+                format24h
+                :with-seconds="withSeconds"
+                :color="color"
+                now-btn
+                @input="log"
+                @click="log"
+              />
+              <!--
+                @click.prevent.capture
+               -->
+            </div>
             <q-carousel
               v-model="slide"
               transition-prev="slide-right"
               transition-next="slide-left"
               swipeable
               animated
-              class="fit bg-transparent"
+              :class="`fit bg-transparent ${splitView?'lt-sm' : ''}`"
             >
               <q-carousel-slide name="date" class="row justify-center content-center q-pa-none fit">
                 <q-date
@@ -72,7 +107,8 @@
                   format24h
                   :with-seconds="withSeconds"
                   :color="color"
-                  @click.prevent.capture
+                  now-btn
+                  @input="log"
                 />
               </q-carousel-slide>
             </q-carousel>
@@ -84,6 +120,7 @@
               color="grey-6"
               :toggle-color="color"
               flat
+              :class="splitView?'lt-sm' : ''"
               :options="[
                 {value: 'date', icon: 'mdi-calendar'},
                 {value: 'time', icon: 'mdi-clock-outline'}
@@ -100,7 +137,8 @@
 </template>
 
 <script>
-import { date } from 'quasar'
+import { date, debounce } from 'quasar'
+
 
 export default {
   name: 'QTimestampPicker',
@@ -136,10 +174,39 @@ export default {
       type: Boolean,
       default: true
     },
+    // z-index for popups
     popupZindex: {
       type: Number,
       default: 6000
     },
+    // allow to edit time by input
+    editableInput: {
+      type: Boolean,
+      default: false
+    },
+    // delay to apply value by manual input -1 = disabled
+    applyInputDelay: {
+      type: Number,
+      default: 2000
+    },
+    // split dialog view for large screens (calendar|time)
+    splitView: {
+      type: Boolean,
+      default: false
+    },
+
+    translations: {
+      type: Object,
+      default: function () {
+        return {
+          __wrong_format_title: 'Wrong date format!',
+          __wrong_format_message: 'Do you want to fix the date and time or set the previous ones?',
+          __wrong_format_ok: 'Fix',
+          __wrong_format_cancel: 'Previous'
+         }
+      }
+    },
+
     withInput: {
       type: Boolean,
       default: true
@@ -150,10 +217,53 @@ export default {
       datetime: date.formatDate(this.value ? (this.value) : Date.now(), this.format),
       proxydatetime: '',
       showdialog: false,
-      slide: 'date'
+      slide: 'date',
+      applyInput: () => {},
+      whatToDo: false
+    }
+  },
+  watch: {
+    datetime (val, old) {
+      if (val !== old) {
+        this.parseDate(val)
+      }
+    }
+  },
+  mounted () {
+    let that = this
+    if (this.applyInputDelay > -1) {
+      this.applyInput = debounce(function () {
+        that.parseDate(that.$refs.input.$refs.input.value)
+      }, this.applyInputDelay)
     }
   },
   methods: {
+    log: console.log,
+    parseDate(val) {
+        let that = this
+        let d = Date.parse(val)
+        if (!Number.isNaN(d)) {
+          this.datetime = date.formatDate(new Date(d), this.format)
+          this.$emit('input', parseInt(date.formatDate(date.extractDate(this.datetime, this.format).getTime(), 'x')))
+        } else {
+          if (!this.whatToDo && this.$q.dialog) {
+            this.whatToDo = true
+            this.$q.dialog({
+              title: this.translations.__wrong_format_title,
+              message: this.translations.__wrong_format_message,
+              ok: this.translations.__wrong_format_ok,
+              cancel: this.translations.__wrong_format_cancel,
+              contentStyle: `z-index:${this.popupZindex}`
+            }).onOk(() => {
+            }).onCancel(() => {
+              this.datetime = date.formatDate(this.value, this.format)
+            }).onDismiss(()=>{
+              setTimeout(function () { that.$refs.input.focus() }, 100)
+              this.whatToDo = false
+            })
+          }
+        }
+    },
     show() {
       if (!this.disable) {
         this.slide = 'date'
